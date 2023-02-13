9f156660c1dc3c05d4870ef5108566b15212da80
Radhwen <radhwen.rmili@esprit.tn>
2023-02-13 16:03:20 +0100

    Add project files.

---
+++ AM.ApplicationCore/Services/ServiceFlight.cs
@@ -0,0 +1,101 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServiceFlight:IServiceFlight
+    {
+        public List<Flight> Flights { get; set; }= new List<Flight>();
+
+        public List<DateTime> GetFlightDates(string destination)
+        {
+            //List<DateTime> result = new List<DateTime>();
+            //for (int i = 0; i < Flights.Count; i++)
+            //{
+  
+            //    if (Flights[i].Destination == "destination")
+            //      result.Add(Flights[i].FlightDate);
+            //}
+            throw new NotImplementedException();
+        }
+
+        public void GetFlights(string filterType, string filterValue)
+        {
+            switch (filterType)
+            {
+                case "destination":
+                    foreach (var item in Flights)
+                    {
+                        if (item.Destination == filterValue)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    break;
+                case "FlightDate":
+                    foreach (var item in Flights)
+                    {
+                        if (item.FlightDate == DateTime.Parse(filterValue))
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    break;
+                case "departure":
+                    foreach (var item in Flights)
+                    {
+                        if (item.Departure == filterValue)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    break;
+                case "Flightid":
+                    foreach (var item in Flights)
+                    {
+                        if (item.FlighID == int.Parse(filterValue))
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                    break;
+
+            }
+        }
+
+
+        public IEnumerable<DateTime> GetFlightdates(string destination)
+        {
+            var query = from flight in Flights
+                        where flight.Destination == destination
+                        select flight.FlightDate;
+            return query;
+        }
+
+        public int ProgrammedFlightNumber(DateTime startDate)
+        {
+            var query = from flight in Flights
+                        where flight.FlightDate > startDate && flight.FlightDate < startDate.AddDays(7)
+                        select flight;
+            return query.Count();
+        }
+
+        public void ShowFlightDetails(Plane plane)
+        {
+            var query = from flight in Flights
+                        where flight.Plane == plane
+                        select flight;
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.FlightDate);
+                Console.WriteLine(item.Destination);
+            }
+        }
+
+    }
+}
---
+++ AM.UI.ConsoleNew/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Services;
+
+
+ServiceFlight serviceFlight= new ServiceFlight();
+serviceFlight.Flights = TestData.listFlights;
+serviceFlight.GetFlights("destination", "Paris");
